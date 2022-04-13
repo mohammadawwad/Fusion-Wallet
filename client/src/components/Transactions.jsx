1e87baa1a14e184e1e7f8b7fd406fb102907dfef
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-04-13 01:49:29 +0000

    dummy transactions page and custom useEffect hook created

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,10 +2,64 @@ import React, {useContext} from 'react';
 import {TransactionContext} from '../context/TransactionContext';
 import DummyData from '../utils/DummyData';
 import {addressShortener} from '../utils/addressShortener.js';
+import useFetch from '../hooks/useFetch';
+
+//card generator for previous transactions
+const TransactionCard = ({addressTo, addressFrom, timestamp, message, amount, url, keyword}) => {
+
+    //custom hook for fetching the gifs
+    const gifURL = useFetch({keyword});
+    
+    return (
+        <div className="bg-[#181918] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] min-w-full flex-col p-3 rounded-md hover:shadow-2xl">
+            <div className="flex flex-col items-center w-full mt-3">
+                <div className="w-full mb-6 p-2">
+                    {/*Shows who it is from and the link to the ropsten network*/}
+                    <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener noreferrer">
+                        <p className="text-white text-base">From: {addressShortener(addressFrom)}</p>
+                    </a>
+
+                    {/*Shows who it is sent to and the link to the ropsten network*/}
+                    <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noopener noreferrer">
+                        <p className="text-white text-base">To: {addressShortener(addressTo)}</p>
+                    </a>
+
+                    {/*Outputs the amount sent*/}
+                    <p className="text-white text-base"> Amount: {amount} ETH</p>
+
+                    {/*the message sent with the specific transaction*/}
+                    {
+                        message && (
+                            <>
+                                <br/>
+                                <p classname="text-white text-base">Message: {message}</p>
+                            </>
+                        )
+                    }
+                </div>
+
+             
+                {/*Rendering the real or dummy image/gif */}
+                <img
+                    src={gifURL || url}
+                    alt="gif"
+                    className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
+                />
+            
+                {/*timestamp sent*/}
+                <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+                    <p className="text-[#37c7da] font-bold">{timestamp}</p>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
 
 const Transactions = () => {
     const {currentAccount} = useContext(TransactionContext);
-     
+    
     return(
         <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
             <div className="flex flex-col md:p-12 py-12 px-4">
@@ -20,7 +74,12 @@ const Transactions = () => {
                 }
 
                 <div className="flex flex-wrap justify-center items-center mt-10">
-                
+                    {/*Rendering dummy data from the latest to the last*/}
+                    {
+                        DummyData.reverse().map((transaction, i) => (
+                            <TransactionCard key={i} {...transaction}/>
+                        ))
+                    }
                 </div>
                 
             </div>
---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react';
+
+//gives access to the api key variable
+const API_KEY = import.meta.env.VITE_GIPHY_API;
+
+//custom useFetch hook
+const useFetch = ({keyword}) => {
+    const [gifURL, setGifURL] = useState("");
+
+    const fetchGifs = async () => {
+        try{
+            //splits words then joins them so it can be searched withouth spaces and only looks for 1 gif
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}$limit=1`);
+
+            //destructuring the data
+            const {data} = await response.json();
+            setGifURL(data[0]?.image?.downsized_medium?.url);
+    
+        } catch(error) {
+            //generates a demo gif if nothing is found
+            setGifURL('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284');
+        }
+    }
+
+    //occurs whenever the keyword changes
+    useEffect(() => {
+        if(keyword){
+            fetchGifs();
+        }
+    }, [keyword])
+
+    return gifURL;
+}
+
+export default useFetch;
